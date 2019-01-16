c2ce9c9318d22de012fdf2c59b4f0a61d51408d7
47582 <475825657@qq.com>
2019-01-16 13:43:55 +0800

    支持沉浸式

--- mylibrary/src/main/java/com/lcg/mylibrary/BaseActivity.kt
+++ mylibrary/src/main/java/com/lcg/mylibrary/BaseActivity.kt
@@ -2,9 +2,12 @@ package com.lcg.mylibrary
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.support.v4.app.FragmentActivity
 import android.view.View
+import android.view.WindowManager
 import com.lcg.mylibrary.dialog.ProgressDialog
 import com.umeng.analytics.MobclickAgent
 import okhttp3.Call
@@ -22,6 +25,8 @@ open class BaseActivity : FragmentActivity() {
     private var mProgressDialog: ProgressDialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        if (translucentStatus)
+            setTranslucentStatus()
         super.onCreate(savedInstanceState)
         activities.add(this)
     }
@@ -84,8 +89,78 @@ open class BaseActivity : FragmentActivity() {
         finish()
     }
 
+    /**
+     * 设置状态栏透明
+     */
+    private fun setTranslucentStatus() {
+        // 5.0以上系统状态栏透明
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val window = window
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = Color.TRANSPARENT
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        }
+    }
+
+    /**
+     * 设置Android状态栏的字体颜色，状态栏为亮色的时候字体和图标是黑色，状态栏为暗色的时候字体和图标为白色
+     *
+     * @param dark 状态栏字体和图标是否为深色
+     */
+    protected fun setStatusBarFontDark(dark: Boolean) {
+        // 小米MIUI
+        try {
+            val window = window
+            val clazz = getWindow().javaClass
+            val layoutParams = Class.forName("android.view.MiuiWindowManager\$LayoutParams")
+            val field = layoutParams.getField("EXTRA_FLAG_STATUS_BAR_DARK_MODE")
+            val darkModeFlag = field.getInt(layoutParams)
+            val extraFlagField = clazz.getMethod("setExtraFlags", Int::class.javaPrimitiveType, Int::class.javaPrimitiveType)
+            if (dark) {    //状态栏亮色且黑色字体
+                extraFlagField.invoke(window, darkModeFlag, darkModeFlag)
+            } else {       //清除黑色字体
+                extraFlagField.invoke(window, 0, darkModeFlag)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        // 魅族FlymeUI
+        try {
+            val window = window
+            val lp = window.attributes
+            val darkFlag = WindowManager.LayoutParams::class.java.getDeclaredField("MEIZU_FLAG_DARK_STATUS_BAR_ICON")
+            val meizuFlags = WindowManager.LayoutParams::class.java.getDeclaredField("meizuFlags")
+            darkFlag.isAccessible = true
+            meizuFlags.isAccessible = true
+            val bit = darkFlag.getInt(null)
+            var value = meizuFlags.getInt(lp)
+            if (dark) {
+                value = value or bit
+            } else {
+                value = value and bit.inv()
+            }
+            meizuFlags.setInt(lp, value)
+            window.attributes = lp
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        // android6.0+系统
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (dark) {
+                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or 8192
+            }
+        }
+    }
+
     companion object {
         @JvmStatic
         var activities = ArrayList<BaseActivity>()
+        @JvmStatic
+        var translucentStatus = false
     }
 }
