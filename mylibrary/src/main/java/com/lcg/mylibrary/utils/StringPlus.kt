b7ef7f9000236a0ba0791450f0f26b2e9c484fb5
86135 <475825657@qq.com>
2022-01-28 16:11:21 +0800

    移除友盟统计

--- mylibrary/src/main/java/com/lcg/mylibrary/utils/StringPlus.kt
+++ mylibrary/src/main/java/com/lcg/mylibrary/utils/StringPlus.kt
@@ -40,13 +40,13 @@ fun String?.containsEmoji(): Boolean {
 }
 
 private fun isEmojiCharacter(codePoint: Char): Boolean {
-    return !(codePoint.toInt() == 0x0 ||
-            codePoint.toInt() == 0x9 ||
-            codePoint.toInt() == 0xA ||
-            codePoint.toInt() == 0xD ||
-            codePoint.toInt() in 0x20..0xD7FF ||
-            codePoint.toInt() in 0xE000..0xFFFD ||
-            codePoint.toInt() in 0x10000..0x10FFFF)
+    return !(codePoint.code == 0x0 ||
+            codePoint.code == 0x9 ||
+            codePoint.code == 0xA ||
+            codePoint.code == 0xD ||
+            codePoint.code in 0x20..0xD7FF ||
+            codePoint.code in 0xE000..0xFFFD ||
+            codePoint.code in 0x10000..0x10FFFF)
 }
 
 /**是否是电话*/
